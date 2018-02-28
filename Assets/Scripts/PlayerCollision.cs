4215a788548d6deac808be9e0377f83c53916e8a
Alyssa Joy Prodigalidad <aprodiga@uci.edu>
2018-02-27 23:39:03 -0800

    add ui, lose and win condition, and levels

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -9,6 +9,8 @@ public class PlayerCollision : MonoBehaviour {
 		if (collisionInfo.collider.tag == "Obstacle") 
 		{
 			movement.enabled = false;
+			FindObjectOfType<GameManager> ().EndGame();
+
 		}
 	}
 }
